d2f48ef28cfebbe3d3543067b340afe9bad11154
tetraxile-ci <tatrexile@gmail.com>
2023-01-17 23:18:54 +0000

    add table initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 import words from './words.js';
 
-console.log("1000th word:", words[999]);
-console.log("word count:", words.length);
+
+document.addEventListener("DOMContentLoaded", function() {
+    initializeTable();
+});
+
+
+
+
+function initializeTable() {
+    let main = document.getElementsByTagName("main")[0];
+
+    // https://stackoverflow.com/a/27646877/12317855
+    let htmlBuffer = [];
+    htmlBuffer.push("<table id='game-board'>");
+    htmlBuffer.push("<tbody>");
+    for (let i = 0; i < 6; i++) {
+        htmlBuffer.push("<tr>");
+        for (let j = 0; j < 5; j++) {
+            htmlBuffer.push("<td></td>");
+        }
+        htmlBuffer.push("</tr>");
+    }
+    htmlBuffer.push("</tbody>");
+    htmlBuffer.push("</table>");
+    console.log(htmlBuffer);
+
+    main.innerHTML = htmlBuffer.join("\n");
+}
