093adfd5f716db2d3e2ba59d3f69e7e370c1c6d0
tetraxile-ci <tatrexile@gmail.com>
2023-01-18 02:16:46 +0000

    add keyboard input to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 import {allowedWords, answerWords} from './words.js';
 
 
+let guesses = 0;
+let currentRowLetters = 0;
+let isGameOver = false;
+let board;
+
+
 document.addEventListener("DOMContentLoaded", function() {
     initializeTable();
+
+    board = document.getElementById("game-board");
+
+    // add event listeners
+    document.addEventListener("keydown", handleKeyInput)
+
 });
-
-
 
 
 function initializeTable() {
@@ -28,3 +38,50 @@ function initializeTable() {
 
     main.innerHTML = htmlBuffer.join("\n");
 }
+
+
+function handleKeyInput(event) {
+    if (isGameOver) return;
+
+    // a guess is only allowed if a 5 letter word is entered
+    if (event.key === "Enter" && currentRowLetters == 5) {
+        makeGuess();
+        return;
+    }
+
+    if (event.key === "Backspace" && currentRowLetters > 0) {
+        let prevCell = getCell(guesses, currentRowLetters - 1);
+        prevCell.innerHTML = "";
+        currentRowLetters--;
+        return;
+    }
+
+    // only allow letter keys
+    const key = event.key.toLowerCase();
+    if (!/^[a-z]$/.test(key)) return;
+
+    // if the current row is full the letter input shouldn't go through
+    if (currentRowLetters >= 5) return;
+
+    let cell = getCell(guesses, currentRowLetters);
+    cell.innerHTML = key;
+    currentRowLetters++;
+}
+
+
+function getCell(row, column) {
+    let tBody = board.children[0];
+    let curRow = tBody.children[row];
+    let curCell = curRow.children[column];
+
+    return curCell;
+}
+
+
+function makeGuess() {
+    guesses++;
+    currentRowLetters = 0;
+
+    if (guesses === 6) isGameOver = true;
+}
+
